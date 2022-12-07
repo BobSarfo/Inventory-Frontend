b78f32e6671e1d6dc409655abb1a1dcb630b33c3
BobSarfo-at-670031179633 <robertsarfo@gmail.com>
2022-12-07 09:56:29 +0000

    configure redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
+import { Provider } from 'react-redux'
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { Toaster } from 'react-hot-toast'
+import store from './setup/redux/Store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Toaster />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
